9b7bdace078122a709a1c70012c7628b5bcf7189
LincolinAngaya <lincolinangaya5626@gmail.com>
2024-04-06 11:40:31 +0300

    modify the locations

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ const people = [
     id: 1,
     image: "/images/person1.jpg",
     name: "Dan Baraka",
-    title: "Multimedia University",
+    title: "Bungalow-Kiserian",
     quote:
       "Spotless Guru, you're my savior! I was broke, but with just a coin in my pocket, I could afford your amazing dish washing services. Quality that fits any budget—thank you for making cleanliness accessible!",
   },
@@ -11,15 +11,15 @@ const people = [
     id: 2,
     image: "/images/person5.avif",
     name: "Rachael Njoroge",
-    title: "Elite Hostel-Karen",
+    title: "Student Hostel-Karen",
     quote:
       "Spotless Guru transformed our hostel kitchen! With efficient dish washing services, they lifted the post-move-in mess, ensuring a spotless and stress-free communal kitchen. Spotless Guru, our hostel's cleaning superhero!",
   },
   {
     id: 3,
     image: "/images/person3.webp",
-    name: "Chiku Adebayo",
-    title: "Tangaza-Karen",
+    name: "Bonface Ameyo",
+    title: "Home Owner-Karen",
     quote:
       "I came from Nigeria to Kenya for my studies, and I was concerned about finding affordable cleaning services while living in the hostels. Being new to the environment, it was important to easily find reliable services. Luckily, it was easier than I expected, and I discovered Spotless Guru Cleaning Services, making my transition much smoother.",
   },
